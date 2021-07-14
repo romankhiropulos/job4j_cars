a0da160d64e2f21569ab036cc52103615faec7b3
roman <romankhiropulos@gmail.com>
2021-07-14 12:44:21 +0300

    first commit

---
+++ src/main/java/ru/job4j/cars/entity/Brand.java
@@ -0,0 +1,10 @@
+package ru.job4j.cars.entity;
+
+public class Brand {
+
+    private int id;
+
+    private String name;
+
+
+}
---
+++ src/main/java/ru/job4j/cars/entity/Model.java
@@ -0,0 +1,9 @@
+package ru.job4j.cars.entity;
+
+public class Model {
+
+    private int id;
+
+    private String name;
+
+}
